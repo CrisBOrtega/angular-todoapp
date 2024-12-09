4b9a82a076591d6c58bdca4b3edb3fff2ec83db3
Elshaman <cbuitragoo.2023@gmail.com>
2024-12-09 06:50:42 -0500

    objetos

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import {Component, signal} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import { Tarea } from './../../models/tarea.model'
 
 @Component({
   selector: 'app-home',
@@ -8,15 +9,39 @@ import {CommonModule} from '@angular/common';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  tareas = signal( [
-    'instalacion',
-    'saturacion'
+  tareas = signal<Tarea[]>( [
+    {
+      id:Date.now(),
+      title: 'Crear proyecto',
+      completed: false
+    },
+    {
+      id:Date.now(),
+      title: 'Crear componentes',
+      completed: false
+    },
+    {
+      id:Date.now(),
+      title: 'Crear css y estilos',
+      completed: false
+    }
   ])
 
   changeHandler(event: Event): void {
     const input  = event.target as HTMLInputElement;
     const nuevaTarea = input.value
-    this.tareas.update((tareas)=>[...tareas , nuevaTarea])
+    this.addTarea(nuevaTarea)
+
+  }
+
+  addTarea(title: string){
+    const newTarea = {
+      id: Date.now(),
+      title: title,
+      completed: false
+    }
+
+    this.tareas.update((tareas)=>[...tareas , newTarea])
   }
 
   deleteTarea(index:number){
